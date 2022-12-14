c580b6bd17d200106a40d1fbc47cb472dd1ca0f2
Amr <akkk33@protonmail.com>
2022-12-14 14:03:11 +0200

    Added dog cube

--- src/lib/Components/Components.svelte
+++ src/lib/Components/Components.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import Contents from './Contents/Contents.svelte';
+	import RotatingDog from './Contents/RotatingDog.svelte';
 </script>
 
 <Contents />
+<RotatingDog />
---
+++ src/lib/Components/Contents/RotatingDog.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { Mesh, BoxGeometry, MeshStandardMaterial } from 'three';
+	import { Three, useFrame, useTexture } from '@threlte/core';
+
+	const rotation = { x: 0, y: 0, z: 0 };
+	useFrame((ctx, delta) => {
+		rotation.x += delta / 10;
+		rotation.y -= delta / 7;
+		rotation.z += delta / 13;
+	});
+</script>
+
+<Three
+	type={Mesh}
+	position={[-10, -15, 30]}
+	rotation={[rotation.x, rotation.y, rotation.z]}
+	geometry={new BoxGeometry(5, 5, 5)}
+	material={new MeshStandardMaterial({
+		map: useTexture('/textures/dog.jpg')
+	})}
+	receiveShadow
+	castShadow
+/>
