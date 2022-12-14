7c65c88549f2ead65654da8b63b4e7ff4c8a4c54
Amr <akkk33@protonmail.com>
2022-12-14 12:57:35 +0200

    Added constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,28 @@
+const SITE_URL = 'https://sohaib23.a3k.me';
+const DOMAIN = 'a3k.me';
+const SITE_NAME = "Happy birthday Sohaib";
+const SITE_DESCRIPTION = 'Sohaib has turned 23 yo.';
+const THEME_COLOR = '#61336b';
+const TWITTER_URERNAME = 'a3k_';
+const TELEGRAM_URERNAME = 'computly';
+const LINKEDIN_URERNAME = 'a3k';
+const FACEBOOK_URERNAME = 'computly';
+const FACEBOOK_APP_ID = '431973582019624';
+const SENTRY_REPORTING_URL =
+	'https://o1128842.ingest.sentry.io/api/6329949/security/?sentry_key=323711c731c14daa9181f1e638bcc49a';
+
+const DRACO_PATH = 'https://www.gstatic.com/draco/v1/decoders/';
+export {
+	SITE_URL,
+	DOMAIN,
+	SITE_NAME,
+	SITE_DESCRIPTION,
+	THEME_COLOR,
+	TWITTER_URERNAME,
+	TELEGRAM_URERNAME,
+	LINKEDIN_URERNAME,
+	FACEBOOK_URERNAME,
+	FACEBOOK_APP_ID,
+	SENTRY_REPORTING_URL,
+	DRACO_PATH
+};
