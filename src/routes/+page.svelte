2cba642a33bad1b987c09fabc7ada3ec5072a1b0
Amr <akkk33@protonmail.com>
2022-12-14 12:57:55 +0200

    Added main page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import { Canvas } from '@threlte/core';
+	import Environment from '$lib/Components/Env/Environment.svelte';
+	import Components from '$lib/Components/Components.svelte';
+	import SEO from './SEO.svelte';
+	import Overlay from '$lib/Components/Overlay/Overlay.svelte';
+	import { sceneIsPlaying } from '$lib/Stores/global';
+	import {
+		cameraPosition,
+		controlsEnabled,
+		ogCameraPosition,
+		ogTargetPosition,
+		targetPosition
+	} from '$lib/Stores/camera';
+
+	let overlayIsVisible = true;
+
+	const startTheShow = async () => {
+		overlayIsVisible = false;
+		sceneIsPlaying.set(true);
+		cameraPosition.set(ogCameraPosition);
+		targetPosition.set(ogTargetPosition);
+		// setTimeout(() => {
+		// 	cameraPosition.set(ogCameraPosition);
+		// 	targetPosition.set(ogTargetPosition);
+		// }, 1000);
+		controlsEnabled.set(true);
+		// setTimeout(() => {
+		// 	controlsEnabled.set(true);
+		// }, 3000);
+	};
+</script>
+
+<SEO />
+
+{#if overlayIsVisible}
+	<Overlay on:startTheShow={startTheShow} />
+{/if}
+<main>
+	<Canvas>
+		<Environment />
+		<Components />
+	</Canvas>
+</main>
+
+<style>
+	main {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		z-index: -1;
+	}
+</style>
